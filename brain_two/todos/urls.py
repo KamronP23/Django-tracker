6bcf53356d65e65a83b557e8fde8b339ae0d0477
Kamron <kamronpoosti@gmail.com>
2023-06-01 11:13:32 -0700

    created url for item update

--- brain_two/todos/urls.py
+++ brain_two/todos/urls.py
@@ -5,7 +5,8 @@ from todos.views import (
     todo_list_create,
     todo_list_update,
     todo_list_delete,
-    todo_item_create
+    todo_item_create,
+    todo_item_update
     )
 
 urlpatterns = [
@@ -14,5 +15,6 @@ urlpatterns = [
     path("create/", todo_list_create, name="todo_list_create"),
     path("<int:id>/edit/", todo_list_update, name="todo_list_update"),
     path("<int:id>/delete/", todo_list_delete, name="todo_list_delete"),
-    path("items/create", todo_item_create, name="todo_item_create")
+    path("items/create", todo_item_create, name="todo_item_create"),
+    path("items/<int:id>edit", todo_item_update, name="todo_item_update")
 ]
